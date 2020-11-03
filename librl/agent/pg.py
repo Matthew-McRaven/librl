e20a710e3a32f5922e8ee936ecde43f54aa3797e
Matthew McRaven <mkm302@georgetown.edu>
2020-11-03 15:41:32 -0800

    Implement task sampling.
    Implement a MAML-like approach where we have a task distribution.
    At each epoch, we sample some tasks from this distribution.
    We train on these tasks, and perfom out update using them.
    This approach should allow us to integrate RL and meta-RL using the same terminology.

--- librl/agent/pg.py
+++ librl/agent/pg.py
@@ -25,7 +25,7 @@ class REINFORCEAgent(nn.Module):
         # Cache the last generated policy, so that we can sample for additional actions.
         self.policy_latest = None
         self.actor_net = actor_net
-        self.actor_loss = losses.VPG()
+        self._actor_loss = losses.VPG()
         self.actor_optimizer = torch.optim.Adam(self.actor_net.parameters(), lr=hypers['alpha'], weight_decay=hypers['l2'])
         self.hypers = hypers
 
@@ -37,12 +37,8 @@ class REINFORCEAgent(nn.Module):
         actions, logprobs, self.policy_latest = self.actor_net(state, toggles)
         return actions, logprobs
 
-    def update(self, state_buffer, action_buffer, reward_buffer, policy_buffer):
-        for i in range(1):
-            self.actor_optimizer.zero_grad()
-            actor_loss = self.actor_loss(state_buffer, action_buffer, reward_buffer, policy_buffer)
-            actor_loss.backward()
-            self.actor_optimizer.step()
+    def actor_loss(self, task):
+        return self._actor_loss(task.state_buffer, task.action_buffer, task.reward_buffer, task.policy_buffer)
 
 # Implement a common framework for all synchronous actor-critic methods.
 # It achieves this versatility by allowing you to specify the policy loss
@@ -59,13 +55,13 @@ class ActorCriticAgent(nn.Module):
         # Trust that the caller gave a reasonable value network.
         self.critic_net = critic_net
         # Goal of our value network (aka the critic) is to make our actual and expected values be equal.
-        self.critic_loss = torch.nn.MSELoss(reduction="sum")
+        self._critic_loss = torch.nn.MSELoss(reduction="sum")
         # TODO: Optimize with something other than ADAM.
         self.critic_optimizer = torch.optim.Adam(self.critic_net.parameters(), lr=hypers['alpha'], weight_decay=hypers['l2'])
 
         self.policy_latest = None
         self.actor_net = actor_net
-        self.actor_loss = actor_loss
+        self._actor_loss = actor_loss
         # TODO: Optimize with something other than ADAM.
         self.actor_optimizer = torch.optim.Adam(self.actor_net.parameters(), lr=hypers['alpha'], weight_decay=hypers['l2'])
 
@@ -79,25 +75,13 @@ class ActorCriticAgent(nn.Module):
         # Don't return policy information, so as to conform with stochastic agents API.
         actions, logprobs, self.policy_latest = self.actor_net(state, toggles)
         return actions, logprobs
+        
+    def actor_loss(self, task):
+        return self._actor_loss(task.state_buffer, task.action_buffer, task.reward_buffer, task.policy_buffer)
 
-    def update(self, state_buffer, action_buffer, reward_buffer, policy_buffer):
-        # Train critic (value) network.
-        # Papers suggest training the critic more often than the actor network.
-        # TODO: vary number of iterations the critic is trained for. 
-        for i in range(20):
-            self.critic_optimizer.zero_grad()
-            # Must reshape states to be a batched 1d array.
-            # TODO: Will need different reshaping for CNN's.
-            states = state_buffer.states
-            states = states.view(*(states.shape[0:2]),-1)
-            critic_loss = self.critic_loss(self.critic_net(states), reward_buffer.rewards)
-            #print(f"Value loss: {value_loss.item()}")
-            critic_loss.backward()
-            self.critic_optimizer.step()
-
-        # Train actor (policy) network.
-        for i in range(1):
-            self.actor_optimizer.zero_grad()
-            actor_loss = self.actor_loss(state_buffer, action_buffer, reward_buffer, policy_buffer)
-            actor_loss.backward()
-            self.actor_optimizer.step()
+    def critic_loss(self, task):
+        # Must reshape states to be a batched 1d array.
+        # TODO: Will need different reshaping for CNN's.
+        states = task.state_buffer.states
+        states = states.view(*(states.shape[0:2]),-1)
+        return self._critic_loss(self.critic_net(states), task.reward_buffer.rewards)
---
+++ librl/task/__init__.py
@@ -0,0 +1,3 @@
+from .distribution import *
+from .sampler import *
+from .prototypes import*
