7740de728291e1ac3cafd83e8fcdfd05b2d08e0a
Matthew McRaven <mkm302@georgetown.edu>
2020-11-26 22:22:46 -0800

    Implement multiple bilinear kernels.
    Add base bilinear class which was two input NN's and takes two args in forward.
    SiameseKernel makes both NN's the same.
    JoinKernel  makes both inputs the same.

--- librl/nn/core/__init__.py
+++ librl/nn/core/__init__.py
@@ -2,3 +2,4 @@ from .cnn import ConvolutionalKernel
 from .mlp import MLPKernel
 from .rnn import RecurrentKernel
 from .sequential import SequentialKernel
+from .bilinear import BilinearKernel, SiameseKernel, JoinKernel
