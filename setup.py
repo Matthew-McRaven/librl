2b8e4ee3196d79ca78791691c2f7eea64f616106
Matthew McRaven <mkm302@georgetown.edu>
2020-12-01 14:13:05 -0800

    Prevent install conflicts between package and tests.
    I had conflicting requirments in my package vs tests pip files.
    This really slowed down testng on Gitlab.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages
 import pkg_resources
 install_reqs = []
 install_reqs.extend(["more-itertools", "overrides", "future", "matplotlib", "scipy", "Pillow"])
-install_reqs.extend(["networkx ~= 2.5", "torch ~= 1.6.0", "torchvision ~= 0.7.0", "pytest ~= 6.1"])
+install_reqs.extend(["torch ~= 1.7.0", "torchvision ~= 0.8.1", "pytest ~= 6.1"])
 setup(name='librl',
-      version='0.2.2',
+      version='0.2.3',
       description='Library for various DRL and DL projects.',
       author='Matthew McRaven',
       author_email='mkm302@georgetown.edu',
