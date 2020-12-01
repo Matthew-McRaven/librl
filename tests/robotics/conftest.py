4aee5cbb920c242294e5a366a150a2af965a5446
Matthew McRaven <mkm302@georgetown.edu>
2020-12-01 14:28:39 -0800

    Silence useless warnings from pybullet ant.

--- tests/robotics/conftest.py
+++ tests/robotics/conftest.py
@@ -13,6 +13,11 @@ def hypers(hypers):
 
 @pytest.fixture()
 def AntEnv(hypers):
+    # Ant environment misbehaves.
+    # Must reduce logging level.
+    # See:
+    #   https://stackoverflow.com/questions/60149105/userwarning-warn-box-bound-precision-lowered-by-casting-to-float32
+    gym.logger.set_level(40)
     env = gym.make("AntBulletEnv-v0")
     env._max_episode_steps = hypers['episode_length']
     return env
