605bec70995b8a82ec2cabf4192e676f6a0ac5b7
Matthew McRaven <mkm302@georgetown.edu>
2020-12-06 23:59:30 -0800

    Enable data limits in classifier training.
    For the sake of unit tests, we would like to train on a subset of an epochs data.
    This allows us to experience more epochs in total.
    
    Secondarily, this will speed integration tests for recognizer.

--- src/librl/task/classification.py
+++ src/librl/task/classification.py
@@ -10,13 +10,17 @@ from .base import Task as _Task
 # with high portability to the testing data.
 # train_data_iter, validation_data_iter should be an iterable of DataLoaders rather than bare DataLoaders.
 class ClassificationTask(_Task):
-    def __init__(self, classifier=None, criterion=None, train_data_iter=None, validation_data_iter=None, train=True, **kwargs):
+    def __init__(self, classifier=None, criterion=None, train_data_iter=None, 
+        validation_data_iter=None, train_percent=1.0, validation_percent=1.0,
+        train=True, **kwargs
+    ):
         super(ClassificationTask, self).__init__(librl.task.ProblemTypes.Classification, **kwargs)
         assert classifier is not None and criterion is not None
         assert (train_data_iter is not None) or (validation_data_iter is not None)
         # Train / validation data should be an iterable of data loaders rather than a single data loader.
         assert not isinstance(train_data_iter, torch.utils.data.DataLoader) # type: ignore
         assert not isinstance(validation_data_iter, torch.utils.data.DataLoader) # type: ignore
+        assert (0.0 < train_percent <= 1.0 and 0.0 < validation_percent <= 1.0)
 
         self.classifier = classifier
         self.criterion = criterion
@@ -24,8 +28,10 @@ class ClassificationTask(_Task):
 
         self.train_batch = train_one_batch
         self.train_data_iter = train_data_iter
+        self.train_percent = train_percent
         self.validate_batch = test_one_batch
         self.validation_data_iter = validation_data_iter
+        self.validation_percent = validation_percent
 
 # Default implementation that trains a classifier on a single batch of data
 def train_one_batch(classifier, criterion, data, target):
--- tests/reco/test_label.py
+++ tests/reco/test_label.py
@@ -14,5 +14,8 @@ def test_label_images(image_dataset, nn_kind, hypers):
     
     # Construct a labelling task.
     dist = librl.task.distribution.TaskDistribution()
-    dist.add_task(librl.task.Task.Definition(librl.task.ClassificationTask, classifier=class_net, criterion=torch.nn.CrossEntropyLoss(), train_data_iter=t, validation_data_iter=v))
+    dist.add_task(librl.task.Task.Definition(librl.task.ClassificationTask, 
+        classifier=class_net, criterion=torch.nn.CrossEntropyLoss(),
+        train_data_iter=t, validation_data_iter=v,train_percent=.2, validation_percent=.1)
+    )
     librl.train.train_loop.cls_trainer(hypers, dist, librl.train.classification.train_single_label_classifier)
